9f20cbc1436412a34cd2b288657d01c1ff426738
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-08-12 20:39:03 +0200

    Update kmlcsvcongacc.R

--- kmlcsvcongacc.R
+++ kmlcsvcongacc.R
@@ -1,17 +1,23 @@
+
 curdir <- getwd()
-setwd(curdir)
+setwd("/home/bigdata09/projs/mob/tt/csvs/")
 
-df0 <- read.csv("kcp1.csv", header=T)
-df1 <- read.csv("kcp2.csv", header=T)
-df2 <- rbind(df0, df1)
 
-df3 <- df2[,-c(2,3,5)]
+df0 <- read.csv("kcp1.csv", header=FALSE)
+df1 <- read.csv("kcp2.csv", header=FALSE)
+colnames(df0) <- c("A","B","C","D","E","F","G","H")
+colnames(df1) <- c("A","B","C","D","E","F","G","H")
+df00 <- df0[,-c(2,3,5,7,8)]
+df11 <- df1[,-c(2,3,5,7,8)]
 
-colnames(df3)[1] <- "city"
-colnames(df3)[2] <- "level"
-colnames(df3)[3] <- "lonlats"
+df2 <- rbind(df00, df11)
 
-df3_ll <- data.frame(strsplit(as.character(df3$lonlats), ','))
+colnames(df2)[1] <- "city"
+colnames(df2)[2] <- "level"
+colnames(df2)[3] <- "lonlats"
+
+df3_ll <- data.frame(strsplit(as.character(df2$lonlats), ','))
 
 accD <- iris[grep("accDelay", df2$city), ]
 congL <- iris[grep("congLevel", df2$city), ]
+

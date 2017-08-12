fdd063c60826cc74e5c6c22abbd7c2c6c946d36a
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-08-12 21:19:16 +0200

    Update kmlcsvcongacc.R

--- kmlcsvcongacc.R
+++ kmlcsvcongacc.R
@@ -2,7 +2,6 @@
 curdir <- getwd()
 setwd("/home/bigdata09/projs/mob/tt/csvs/")
 
-
 df0 <- read.csv("kcp1.csv", header=FALSE)
 df1 <- read.csv("kcp2.csv", header=FALSE)
 colnames(df0) <- c("A","B","C","D","E","F","G","H")
@@ -17,7 +16,12 @@ colnames(df2)[2] <- "level"
 colnames(df2)[3] <- "lonlats"
 
 df3_ll <- data.frame(strsplit(as.character(df2$lonlats), ','))
+df3_ll2 <- data.frame(t(df3_ll))
 
-accD <- iris[grep("accDelay", df2$city), ]
-congL <- iris[grep("congLevel", df2$city), ]
+
+df_acc <- df3[grep("accDelay", rownames(df3)), ]
+dfcon <- df3[grep("congLevel", rownames(df3)), ]
+
+df_acc <- iris[grep("accDelay", df2$city), ]
+dfcon <- iris[grep("congLevel", df2$city), ]
 

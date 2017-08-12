9ebdcad7755cae5672fd7c42f454a896c7d1a4d7
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-08-12 23:08:10 +0200

    Update kmlcsvcongacc.R

--- kmlcsvcongacc.R
+++ kmlcsvcongacc.R
@@ -1,4 +1,3 @@
-
 curdir <- getwd()
 setwd("/home/bigdata09/projs/mob/tt/csvs/")
 
@@ -15,13 +14,21 @@ colnames(df2)[1] <- "city"
 colnames(df2)[2] <- "level"
 colnames(df2)[3] <- "lonlats"
 
-df3_ll <- data.frame(strsplit(as.character(df2$lonlats), ','))
-df3_ll2 <- data.frame(t(df3_ll))
+df2$ll <- sub(',*\\,', '', df2$lonlats)
 
+df3_ll <- data.frame(strsplit(as.character(df2$ll), ','))
+df3_ll2 <- data.frame(t(df3_ll), stringsAsFactors = TRUE)
+
+df3a <- df3_ll2$lon_start
+df3b <- df3_ll2$lat_start
+df3c <- df3_ll2$lon_end
+df3d <- df3_ll2$lat_end 
+
+colnames(df3_ll2)[1] <- "a"
+df3_ll3 <- df3_ll2[,c(2,3,4,5)]
 
 df_acc <- df3[grep("accDelay", rownames(df3)), ]
 dfcon <- df3[grep("congLevel", rownames(df3)), ]
 
-df_acc <- iris[grep("accDelay", df2$city), ]
-dfcon <- iris[grep("congLevel", df2$city), ]
-
+df_acc <- df3[grep("accDelay", df2$city), ]
+dfcon <- df3[grep("congLevel", df2$city), ]

7714bce761c791ece0762b9bee2e1e134f4b9a37
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-08-13 15:29:34 +0200

    Update kmlcsvcongacc.R

--- kmlcsvcongacc.R
+++ kmlcsvcongacc.R
@@ -14,16 +14,53 @@ colnames(df2)[1] <- "city"
 colnames(df2)[2] <- "level"
 colnames(df2)[3] <- "lonlats"
 
+lonlats <- as.data.frame(as.character(df2$lonlats))
+
+lonlats$a <- str_split(lonlats, ",")[[1]]
+lonlats$b <- str_split_fixed(lonlats, ",", 2)
+
+str_split_fixed(",", fixed("..."), 2)
+b <- strsplit(as.character(lonlats),',') 
+ 
+
+lonlats$a <- sub(',*\\,', '', lonlats)
+df2$llx <- sub('.*\\,', '', df2$ll4)
+
+
+
+
+df2$ll2 <- read.table(df2$ll, sep = ",", as.is = TRUE)
+
+df2$ll3 <- sapply(strsplit(df2$ll, ","), "[", 2)
+
 df2$ll <- sub(',*\\,', '', df2$lonlats)
+
+df2$ll1 <- sub('*\\,', '', df2$ll)
+df2$ll2 <- sub(',*\\,', '', df2$lonlats)
+df2$ll3 <- sub(',*\\,', '', df2$lonlats)
+df2$ll4 <- sub(',*\\,', '', df2$lonlats)
 
 df3_ll <- data.frame(strsplit(as.character(df2$ll), ','))
 df3_ll2 <- data.frame(t(df3_ll), stringsAsFactors = TRUE)
 
+==
+df2$ll <- sub(',*\\,', '', df2$lonlats)
+
+df3_ll <- data.frame(strsplit(as.character(df2$ll), ','))
+df3_ll2 <- data.frame(t(df3_ll), stringsAsFactors = TRUE)
+===
+
+colnames(df3_ll2) <- c("lon_start","lat_start","lon_end","lat_end")
+
 df3a <- df3_ll2$lon_start
 df3b <- df3_ll2$lat_start
 df3c <- df3_ll2$lon_end
-df3d <- df3_ll2$lat_end 
+df3d <- df3_ll2$lat_end
 
+dft1 <- data.frame(df3a,df3b)
+dft2 <- data.frame(dft1,df3c)
+dft3 <- data.frame(dft2,df3d)
+ 
 colnames(df3_ll2)[1] <- "a"
 df3_ll3 <- df3_ll2[,c(2,3,4,5)]
 
